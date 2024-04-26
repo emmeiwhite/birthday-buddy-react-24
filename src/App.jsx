aa53066402686ef2353b42d71f8d0275a6001a60
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-26 18:11:56 +0530

    Birth-Buddy with Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <main>
+      <section className="container">
+        <h1 className="text-3xl font-bold font-roboto underline">
+          Hello world!
+        </h1>
+      </section>
+    </main>
+  );
+};
+export default App;
